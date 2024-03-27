db1cbeb8dc2494f38c139a1c5ac339c780d14604
dongwoo <dwseo96@naver.com>
2024-03-28 00:35:53 +0900

    파이썬 기초 100제 - 2467 주사위 게임 푸는 중

--- python/3009_네 번째 점.py
+++ python/3009_네 번째 점.py
@@ -7,10 +7,20 @@
 # 출력
 # 직사각형의 네 번째 점의 좌표를 출력한다.
 
-x1, y1 = map(int,input().split())
-x2, y2 = map(int,input().split())
-x3, y3 = map(int,input().split())
+x_nums = []
+y_nums = []
+x4 = 0
+y4 = 0
+for i in range(3):
+    x,y = map(int,input().split())
+    x_nums.append(x)
+    y_nums.append(y)
 
-print(x1,y1)
-print(x2,y2)
-print(x3,y3)
+for i in range(3):
+    if x_nums.count(x_nums[i]) == 1:
+        x4 = x_nums[i]
+    if y_nums.count(y_nums[i]) == 1:
+        y4 = y_nums[i]
+
+
+print(x4,y4)
