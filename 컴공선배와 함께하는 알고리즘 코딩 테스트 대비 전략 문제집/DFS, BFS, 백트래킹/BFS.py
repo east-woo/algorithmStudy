d5ca0b3bf0ac942cb3817a015512c036c5dc9222
dongwoo <dwseo96@naver.com>
2024-07-19 00:08:47 +0900

    BFS 연습중

--- 컴공선배와 함께하는 알고리즘 코딩 테스트 대비 전략 문제집/DFS, BFS, 백트래킹/BFS.py
+++ 컴공선배와 함께하는 알고리즘 코딩 테스트 대비 전략 문제집/DFS, BFS, 백트래킹/BFS.py
@@ -1,11 +1,11 @@
 import sys
 from collections import deque
 
-dx = (1,0,-1,0)
-dy = (0,1,0,-1)
-chk = [[False]*100 for _ in range(100)]
+dx = (1, 0, -1, 0)
+dy = (0, 1, 0, -1)
+chk = [[False] * 100 for _ in range(100)]
 
-N = int(sys.stdin.readline().strip())
+N = 3
 
 def is_valid_coord(y, x):
     return 0 <= y < N and 0 <= x < N
@@ -16,11 +16,14 @@ def bfs(start_y, start_x):
     while len(q) > 0:
         y, x = q.popleft()
         chk[y][x] = True
-        print(f"Visiting: ({y}, {x})")  # 방문한 좌표 출력
+        print(f"{y}, {x}")
+        print(f"Visiting: ({x}, {y})")  # 방문한 좌표 출력
+
         for k in range(4):
             ny = y + dy[k]
             nx = x + dx[k]
             if is_valid_coord(ny, nx) and not chk[ny][nx]:
                 q.append((ny, nx))
-
-bfs(1,1)
+                chk[ny][nx] = True  # 새로운 좌표를 큐에 넣을 때 방문 체크도 함께 해야 함
+                print(f"{ny}, {nx}")
+bfs(0, 0)
