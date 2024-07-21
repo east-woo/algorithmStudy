ead8a2b21086d27ca2a05f643257326efd6d097a
dongwoo <dwseo96@naver.com>
2024-07-22 00:04:42 +0900

    2512_예산.py 10815_숫자 카드.py 이진탐색.py 문제 해결

---
+++ 컴공선배와 함께하는 알고리즘 코딩 테스트 대비 전략 문제집/이분탐색/10815_숫자 카드.py
@@ -0,0 +1,31 @@
+# 문제
+# 숫자 카드는 정수 하나가 적혀져 있는 카드이다. 상근이는 숫자 카드 N개를 가지고 있다. 정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 가지고 있는지 아닌지를 구하는 프로그램을 작성하시오.
+#
+# 입력
+# 첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다. 두 숫자 카드에 같은 수가 적혀있는 경우는 없다.
+#
+# 셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 넷째 줄에는 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 M개의 정수가 주어지며, 이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다
+#
+# 출력
+# 첫째 줄에 입력으로 주어진 M개의 수에 대해서, 각 수가 적힌 숫자 카드를 상근이가 가지고 있으면 1을, 아니면 0을 공백으로 구분해 출력한다.
+
+import sys
+from bisect import bisect_left,bisect_right
+input = sys.stdin.readline
+
+sang = int(input())
+sang_card = sorted(list(map(int, input().split())))
+m = int(input())
+qry = list(map(int, input().split()))
+
+ans = list()
+def is_existence(data):
+    return bisect_right(sang_card, data)-bisect_left(sang_card,data)
+
+for data in qry:
+    if is_existence(data):
+        ans.append(1)
+    else:
+        ans.append(0)
+
+print(' '.join(map(str,ans)))
