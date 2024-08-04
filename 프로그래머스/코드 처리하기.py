7faa238bfe117cd477eca973aecbcb80d5244e43
dongwoo <dwseo96@naver.com>
2024-08-04 23:19:12 +0900

    코드 처리하기.py 문제 해결 중

--- 프로그래머스/코드 처리하기.py
+++ 프로그래머스/코드 처리하기.py
@@ -17,3 +17,42 @@
 # 제한사항
 # 1 ≤ code의 길이 ≤ 100,000
 # code는 알파벳 소문자 또는 "1"로 이루어진 문자열입니다.
+# 입출력 예
+# code	result
+# "abc1abc1abc"	"acbac"
+# 입출력 예 설명
+# 입출력 예 #1
+#
+# code의 각 인덱스 i에 따라 다음과 같이 mode와 ret가 변합니다.
+# i	code[i]	mode	ret
+# 0	"a"	0	"a"
+# 1	"b"	0	"a"
+# 2	"c"	0	"ac"
+# 3	"1"	1	"ac"
+# 4	"a"	1	"ac"
+# 5	"b"	1	"acb"
+# 6	"c"	1	"acb"
+# 7	"1"	0	"acb"
+# 8	"a"	0	"acba"
+# 9	"b"	0	"acba"
+# 10	"c"	0	"acbac"
+# 따라서 "acbac"를 return 합니다.
+
+def solution(code):
+    answer = ''
+    count = 0
+    mode = 0
+    for i in code:
+        if i == 0 :
+            mode =0
+        elif i == 1:
+            mode = 1
+        if mode == 0 and count %2==0:
+            answer += i
+        elif mode == 1 and count %2==1:
+            answer += i
+        count += 1
+    return answer
+
+
+print(solution("abc1abc1abc"))
